3e8e36e2db265aa996fd5692baf025bffe912dac
salma elsayed <ssayed.yousef@gmail.com>
2020-04-20 01:52:21 +0200

    semaphores working

--- Copy of OSProj/src/OperatingSystem.java
+++ Copy of OSProj/src/OperatingSystem.java
@@ -10,7 +10,17 @@ import java.util.concurrent.Semaphore;
 
 public class OperatingSystem {
 	
+	static boolean  readSem  =true;
+	static boolean  writeSem =true;
+	static boolean  printSem =true;
+	static boolean  inputSem =true;
+	
+	
 	public static ArrayList<Thread> ProcessTable;
+	public static ArrayList<Thread> printBlockedProcesses = new ArrayList<Thread>();
+	public static ArrayList<Thread> inputBlockedProcesses = new ArrayList<Thread>();
+	public static ArrayList<Thread> readBlockedProcesses = new ArrayList<Thread>();
+	public static ArrayList<Thread> writeBlockedProcesses = new ArrayList<Thread>();
 
 //	public static int activeProcess= 0;
 	//system calls:
@@ -69,10 +79,124 @@ public class OperatingSystem {
 		Process p = new Process(processID);
 		ProcessTable.add(p);
 		Process.setProcessState(p,ProcessState.Ready);
-		p.start();
-		
+//		if(ProcessTable.size()>1) {
+//		try {
+//			p.join();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		}
+//		else {
+			p.start();
+//		}
 	}
 	
+	public static void semPrintWait(Process p){
+//		System.out.println("waiting "+printSem);
+		while(!printSem) {
+//			System.out.println("suspension");
+			printBlockedProcesses.add(p);
+			Process.setProcessState(p, ProcessState.Waiting);
+			p.suspend();		
+		}
+		printSem=false;
+//		System.out.println("checkprint "+printSem);
+
+	}
+	
+	public static void semPrintPost(Process p){
+//		System.out.println("posting "+printSem);
+		if (!printSem && printBlockedProcesses.size()>0) {
+		Process p2 = (Process) printBlockedProcesses.get(0);
+		p2.resume();
+		Process.setProcessState(p2, ProcessState.Ready);
+		printBlockedProcesses.remove(0);
+		}
+		printSem = true;
+
+	}
+	
+	public static void semInputWait(Process p){
+//		System.out.println("waiting "+printSem);
+		while(!inputSem) {
+//			System.out.println("suspension");
+			inputBlockedProcesses.add(p);
+			Process.setProcessState(p, ProcessState.Waiting);
+			p.suspend();		
+		}
+//		else {
+			inputSem=false;
+//			}
+//		System.out.println("checkprint "+printSem);
+
+	}
+	
+	public static void semInputPost(Process p){
+//		System.out.println("posting "+printSem);
+		if (!inputSem && inputBlockedProcesses.size()>0) {
+		Process p2 = (Process) inputBlockedProcesses.get(0);
+		p2.resume();
+		Process.setProcessState(p2, ProcessState.Ready);
+		inputBlockedProcesses.remove(0);
+		}
+		inputSem = true;
+
+	}
+	
+	public static void semReadWait(Process p){
+//		System.out.println("waiting "+printSem);
+		while(!readSem) {
+//			System.out.println("suspension");
+			readBlockedProcesses.add(p);
+			Process.setProcessState(p, ProcessState.Waiting);
+			p.suspend();		
+		}
+//		else {
+			readSem=false;
+//			}
+		System.out.println("checkread "+readSem);
+
+	}
+	
+	public static void semReadPost(Process p){
+//		System.out.println("posting "+printSem);
+		if (!readSem && readBlockedProcesses.size()>0) {
+		Process p2 = (Process) readBlockedProcesses.get(0);
+		p2.resume();
+		Process.setProcessState(p2, ProcessState.Ready);
+		readBlockedProcesses.remove(0);
+		}
+		readSem = true;
+
+	}
+	
+	public static void semWriteWait(Process p){
+//		System.out.println("waiting "+printSem);
+		while(!writeSem) {
+//			System.out.println("suspension");
+			writeBlockedProcesses.add(p);
+			Process.setProcessState(p, ProcessState.Waiting);
+			p.suspend();		
+		}
+//		else {
+			writeSem=false;
+//			}
+//		System.out.println("checkprint "+printSem);
+
+	}
+	
+	public static void semWritePost(Process p){
+		System.out.println("write posting "+writeSem);
+		if (!writeSem && writeBlockedProcesses.size()>0) {
+		Process p2 = (Process) writeBlockedProcesses.get(0);
+		p2.resume();
+		Process.setProcessState(p2, ProcessState.Ready);
+		writeBlockedProcesses.remove(0);
+		}
+		writeSem = true;
+
+	}
 	public static void main(String[] args) {
    		ProcessTable = new ArrayList<Thread>();
 
--- Copy of OSProj/src/Process.java
+++ Copy of OSProj/src/Process.java
@@ -25,48 +25,100 @@ public class Process extends Thread {
 	}
 	
 	private void process1() {
-		
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter File Name: ");
-		OperatingSystem.printText(OperatingSystem.readFile(OperatingSystem.TakeInput()));
+		OperatingSystem.semPrintPost(this);
+
+		
+		
+		OperatingSystem.semInputWait(this);
+		String fname = OperatingSystem.TakeInput();
+		OperatingSystem.semInputPost(this);
+	
+		OperatingSystem.semReadWait(this);
+		String data = OperatingSystem.readFile(fname+".txt");
+		OperatingSystem.semReadPost(this);
+		
+		OperatingSystem.semPrintWait(this);
+		OperatingSystem.printText(data);
+		OperatingSystem.semPrintPost(this);
 		
 		setProcessState(this,ProcessState.Terminated);
+		System.out.println("p1 over");
 		}
 	
 	private void process2() {
 		
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter File Name: ");
+		OperatingSystem.semPrintPost(this);
+
+		OperatingSystem.semInputWait(this);
 		String filename= OperatingSystem.TakeInput();
+		OperatingSystem.semInputPost(this);
+
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter Data: ");
+		OperatingSystem.semPrintPost(this);
+
+		OperatingSystem.semInputWait(this);
 		String data= OperatingSystem.TakeInput();
+		OperatingSystem.semInputPost(this);
+
+		OperatingSystem.semWriteWait(this);
 		OperatingSystem.writefile(filename,data);
+		OperatingSystem.semWritePost(this);
+		
 		setProcessState(this,ProcessState.Terminated);
+		System.out.println("p2 over");
+
 		}
 	private void process3() {
 		int x=0;
+		OperatingSystem.semPrintWait(this);
+
 		while (x<301)
-		{ 
+		{ 			
 			OperatingSystem.printText(x+"\n");
 			x++;
-		}
+		}	
+		OperatingSystem.semPrintPost(this);
 		setProcessState(this,ProcessState.Terminated);
-		}
+		System.out.println("p3 over");
+	
+	}
 	
 	private void process4() {
 	
 		int x=500;
+		OperatingSystem.semPrintWait(this);
 		while (x<1001)
 		{
+		
 			OperatingSystem.printText(x+"\n");
 			x++;
 		}	
+		OperatingSystem.semPrintPost(this);
 		setProcessState(this,ProcessState.Terminated);
-		}
+		System.out.println("p4 over");
+}
 	private void process5() {
-		
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter LowerBound: ");
+		OperatingSystem.semPrintPost(this);
+
+		OperatingSystem.semInputWait(this);
 		String lower= OperatingSystem.TakeInput();
+		OperatingSystem.semInputPost(this);
+
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter UpperBound: ");
+		OperatingSystem.semPrintPost(this);
+
+		OperatingSystem.semInputWait(this);
 		String upper= OperatingSystem.TakeInput();
+		OperatingSystem.semInputPost(this);		
+		
 		int lowernbr=Integer.parseInt(lower);
 		int uppernbr=Integer.parseInt(upper);
 		String data="";
@@ -75,9 +127,16 @@ public class Process extends Thread {
 		{
 			data+=lowernbr++ +"\n";
 		}	
+		
+		OperatingSystem.semWriteWait(this);
 		OperatingSystem.writefile("P5.txt", data);
+		OperatingSystem.semWritePost(this);
+
 		setProcessState(this,ProcessState.Terminated);
+		System.out.println("p5 over");
 	}
+	
+
 	
 	 public static void setProcessState(Process p, ProcessState s) {
 		 p.status=s;
@@ -90,4 +149,22 @@ public class Process extends Thread {
 	 public static ProcessState getProcessState(Process p) {
 		 return p.status;
 	}
+	 
+//		public void semPrintWait(boolean printSem){
+//			System.out.println("waiting "+printSem);
+//			if(!printSem) {
+//				System.out.println("suspension");
+//				setProcessState(this, ProcessState.Waiting);
+//				this.suspend();		
+//			}else {
+//			printSem=false;}
+//			System.out.println("checkprint "+printSem);
+//
+//		}
+//		
+//		public void semPrintPost(boolean printSem){
+//			System.out.println("posting "+printSem);
+//			printSem = true;
+//			setProcessState(this, ProcessState.Ready);
+//		}
 }
